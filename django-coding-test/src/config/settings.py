bb79c053b10a619b206b3fb31db5c3a9234ce0e7
nurnobiCSE <shuvo517942@gmail.com>
2023-01-18 22:32:42 +0600

    second commit

--- django-coding-test/src/config/settings.py
+++ django-coding-test/src/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'authentication',
-    'product'
+    'product',
+ 
 ]
 
 MIDDLEWARE = [
@@ -120,9 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / '/static/'
 ]
+
 
 MEDIA_URL = '/media/'
 
